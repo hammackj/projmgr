dbbbfa3118e8af79db9cc423d7ef84b4251683d8
Jacob Hammack <jacob.hammack@hammackj.com>
2010-12-27 20:02:16 -0600

    Updated the commands to be executed with `` instead of system

--- lib/projmgr/svn.rb
+++ lib/projmgr/svn.rb
@@ -1,17 +1,18 @@
+require 'projmgr/scm'
 
 module ProjMgr
 	
 	class Svn < Scm
 		
 		def update
-		  results = system('cd #{prefix}/#{project}  && svn stat && svn update && cd #{@root}')
+		  results = `cd #{@path}  && svn stat && svn update && cd #{@root}`
 		  puts results
 		end
 	
 		def has_local_changes?
-			results = system('cd #{@prefix}/#{@project} && svn stat && cd #{@root}')
+			results = `cd #{@path} && svn stat && cd #{@root}`
 			
-			if results.lenght > 0
+			if results.length > 0
 				return true
 			else
 				return false
