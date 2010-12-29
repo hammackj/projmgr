756988d930ccfcf3a0f83cef5c00e6ef84a1f34b
Jacob Hammack <jacob.hammack@hammackj.com>
2010-12-28 20:10:28 -0600

    Added comments

--- lib/projmgr/scm.rb
+++ lib/projmgr/scm.rb
@@ -1,12 +1,15 @@
-#
-#
 module ProjMgr
 	
+	# A parent class for interacting with a source code repository
 	#
-	#
+	# @author Jacob Hammack <jacob.hammack@hammackj.com>
 	class Scm
 		
+		# Creates a instance of a SCM repository
 		#
+		# @param project name of the project
+		# @param path path to the project
+		# @param root path back to the root
 		#
 		def initialize project, path, root
 			@project = project
