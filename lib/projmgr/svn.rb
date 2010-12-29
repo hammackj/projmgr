106ef2f1e9bfc1b3e9703251c677d006e0c8e8d4
Jacob Hammack <jacob.hammack@hammackj.com>
2010-12-28 20:10:51 -0600

    Added comments and changed the return on update as a result

--- lib/projmgr/svn.rb
+++ lib/projmgr/svn.rb
@@ -2,13 +2,22 @@ require 'projmgr/scm'
 
 module ProjMgr
 	
+	# A parent class for interacting with a source code repository
+	#
+	# @author Jacob Hammack <jacob.hammack@hammackj.com>
 	class Svn < Scm
 		
+		# Checks for updates in the target repo
+		# 
+		# @return [String] the results of 'git pull' on the target repository
 		def update
 		  results = `cd #{@path}  && svn stat && svn update && cd #{@root}`
-		  puts results
+		  return results
 		end
-	
+
+		# Checks for local changes in the target repository
+		#
+		# @return [Boolean] if there is local changes or not
 		def has_local_changes?
 			results = `cd #{@path} && svn stat && cd #{@root}`
 			
@@ -18,7 +27,5 @@ module ProjMgr
 				return false
 			end
 		end
-	
 	end
-
 end
