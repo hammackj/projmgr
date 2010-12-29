72988a8d3ac8cd9dad3e7dbec0931655f5edd959
Jacob Hammack <jacob.hammack@hammackj.com>
2010-12-28 20:09:27 -0600

    Added comments and changed the return on update as a result

--- lib/projmgr/git.rb
+++ lib/projmgr/git.rb
@@ -1,27 +1,28 @@
 require 'projmgr/scm'
 
-#
-#
 module ProjMgr
 	
+	# A wrapper class for interacting with a git repository
 	#
-	#
+	# @author Jacob Hammack <jacob.hammack@hammackj.com>
 	class Git < Scm
 
-		#
-		#
+		# Checks for updates in the target repo
+		# 
+		# @return [String] the results of 'git pull' on the target repository
 		def update
 	  	results = `cd #{@path} && git pull && cd #{@root}`
 
 	  	if results =~ /Already up-to-date./
-	  	  print "Already up-to-date!\n"
+	  	  return "Already up-to-date!\n"
 	  	else
-	  	  puts results  	  
+	  	  return results  	  
 		  end
 		end
 		
+		# Checks for local changes in the target repository
 		#
-		#
+		# @return [Boolean] if there is local changes or not
 		def has_local_changes?
 		  results = `cd #{@path} && git status && cd #{@root}`
 
