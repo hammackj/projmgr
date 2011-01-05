8a75e2a4f9613fdc3594634763775d908b8a99d4
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-05 16:56:22 -0600

    Changed some require's around and updated the version

--- lib/projmgr.rb
+++ lib/projmgr.rb
@@ -1,13 +1,12 @@
+# encoding: utf-8
 
 module ProjMgr
 	APP_NAME = "projmgr"
-	VERSION = "0.0.2"
+	VERSION = "0.0.3"
 	CONFIG_FILE = "~/.projmgr"
 end
 
-require 'choice'
-require 'yaml'
-
+require 'projmgr/scm'
 require 'projmgr/git'
 require 'projmgr/svn'
-require 'projmgr/scm'
+require 'projmgr/cvs'
