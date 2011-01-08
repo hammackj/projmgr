e8725807943cde0e90d9ffb53a386ab748d7273d
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-08 15:00:14 -0600

    Changed returns and fixed some case on the wording, also added error checking

--- lib/projmgr/git.rb
+++ lib/projmgr/git.rb
@@ -15,42 +15,53 @@ module ProjMgr
 		# @return [String] The results from the 'git clone' command
 		def checkout
 			if path_exists? == true
-				return "Path Exists, cannot checkout onto an existing repo"
+				return "path exists, cannot checkout onto an existing repo"
 			else
 				parent = project_parent_directory
-				results = `cd #{parent} && git clone #{@url} && cd #{@root}`
+				
+				cmd = IO.popen "cd #{parent} && git clone #{@url} &> /dev/null && cd #{@root}"
+				results = cmd.readlines
+				cmd.close
+				
+				return "project checked out to #{parent}/#{@project}"
 			end
-			
-			return results
 		end
 
-		# Checks for updates in the target repo
+		# Checks for updates in the target repository
 		# 
 		# @return [String] the results of 'git pull' on the target repository
 		def update
-	  	results = `cd #{@path} && git pull && cd #{@root}`
-
-	  	if results =~ /Already up-to-date./
-	  	  return "Already up-to-date!\n"
-	  	else
-	  	  return results  	  
-		  end
+			if path_exists? == false
+				return "path does not exists, cannot update repository"
+			else
+	  		results = `cd #{@path} && git pull && cd #{@root}`
+			
+		  	if results =~ /Already up-to-date./
+		  	  return "Already up-to-date!\n"
+		  	else
+		  	  return results  	  
+			  end
+			end
 		end
 		
 		# Checks for local changes in the target repository
 		#
 		# @return [Boolean] if there is local changes or not
 		def has_local_changes?
-		  results = `cd #{@path} && git status && cd #{@root}`
+			if path_exists? == false				
+				return false, "path does not exists, please check the path or check it out"
+			else
+			  results = `cd #{@path} && git status && cd #{@root}`
 
-		  if results !~ /nothing to commit/ 
-				return true
-			elsif results =~ /Your branch is ahead of/
-				return true
-			elsif results =~ /Untracked files/
-				return true
-		  else
-				return false
+			  if results !~ /nothing to commit/ 
+					return true, "has local changes"
+				elsif results =~ /Your branch is ahead of/
+					return true, "has local changes"
+				elsif results =~ /Untracked files/
+					return true, "has local changes"
+			  else
+					return false, "has no local changes"
+				end
 			end
 		end
 	end	
