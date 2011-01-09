329e0b2055e8f59b646deeb3ca17d203d58828d3
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-08 22:09:17 -0600

    Added path checking, and more detailed return messages

--- lib/projmgr/svn.rb
+++ lib/projmgr/svn.rb
@@ -13,29 +13,48 @@ module ProjMgr
 		#
 		# @return [String] The results from the 'svn checkout' command
 		def checkout
-			results = `cd #{@path} && cd .. && svn checkout #{@url} && cd #{@root}`
-			
-			return results
+			if path_exists? == true
+				return "path exists, cannot checkout onto an existing repo"
+			else		
+				parent = project_parent_directory	
+				
+				results = `cd #{parent} && svn checkout #{@url} #{@project} && cd #{@root}`
+				
+				if results =~ /Could not resolve hostname/
+					return "unable to resolve hostname"
+				else
+					return "project checked out to #{parent}/#{@project}"
+				end
+			end
 		end
 		
 		# Checks for updates in the target repo
 		# 
 		# @return [String] the results of 'git pull' on the target repository
 		def update
-		  results = `cd #{@path} && svn stat && svn update && cd #{@root}`
-		  return results
+			if path_exists? == true
+		  	results = `cd #{@path} && svn stat && svn update && cd #{@root}`
+			else		
+				return "path does not exists, cannot update repository"
+		  end
+		
+			return results
 		end
 
 		# Checks for local changes in the target repository
 		#
 		# @return [Boolean] if there is local changes or not
 		def has_local_changes?
-			results = `cd #{@path} && svn stat && cd #{@root}`
-			
-			if results.length > 0
-				return true
-			else
-				return false
+			if path_exists? == false
+				return false, "Path does not exists, please check the path or check it out"
+			else 
+				results = `cd #{@path} && svn stat && cd #{@root}`
+								
+				if results.length > 0
+					return true, "has local changes"
+				else
+					return false, "has no local changes"
+				end
 			end
 		end
 	end
