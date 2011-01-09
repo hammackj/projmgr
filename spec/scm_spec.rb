42cf7503cea6bdb4d7c0f38a0d4dfb8483ec73f8
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-09 10:23:23 -0600

    Updated the tests to use more of the rspec helpers so they are cleaner

--- spec/scm_spec.rb
+++ spec/scm_spec.rb
@@ -4,26 +4,32 @@ $:.unshift(File.join(File.dirname(base), '../lib'))
 require 'projmgr'
 
 module ProjMgr
-  describe "Scm", ".new" do
-    it "returns Scm Object Scm.new" do
-			scm = Scm.new
-      scm.class.should == ProjMgr::Scm
-    end
-  end
-
-	describe "Scm", ".path_exists?" do
-		it "returns false for /tmp/projmgr/doesntexist" do
-			scm = Scm.new nil, '/tmp/projmgr/doesntexist', nil, nil
-			scm.path_exists?.should == false
+	describe Scm do
+		before(:each) do
+			@scm = Scm.new
 		end
-	end
 	
-	describe "Scm", ".project_parent_directory" do
-		it "returns /tmp for /tmp/projmgr_test" do
-			`mkdir -p /tmp/projmgr_test`
+	  describe Scm, ".new" do
+	    it "returns Scm Object Scm.new" do
+				@scm.class.should == ProjMgr::Scm
+	    end
+	  end
+
+		describe Scm, ".path_exists?" do
+			it "returns false for /tmp/projmgr/doesntexist" do
+				#@scm = Scm.new nil, '/tmp/projmgr/doesntexist', nil, nil
+				@scm.path = '/tmp/projmgr/doesntexist'
+				@scm.path_exists?.should == false
+			end
+		end
+	
+		describe Scm, ".project_parent_directory" do
+			it "returns /tmp for /tmp/projmgr_test" do
+				`mkdir -p /tmp/projmgr_test`
 			
-			scm = Scm.new nil, '/tmp/projmgr_test', nil, nil
-			scm.project_parent_directory.should == '/tmp'
+				@scm = Scm.new nil, '/tmp/projmgr_test', nil, nil
+				@scm.project_parent_directory.should == '/tmp'
+			end
 		end
 	end
 end
